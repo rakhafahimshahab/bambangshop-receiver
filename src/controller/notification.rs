b4a613ae50b5a8aa358c3ff75b7b1d45e6f5de01
rakhafahimshahab <rakha.fahim@ui.ac.id>
2024-04-17 13:36:12 +0700

    Implement subscribe function in Notification service.

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -1,6 +1,6 @@
 use rocket::serde::json::Json;
 
-use bambangshop_receiover::Result;
+use bambangshop_receiver::Result;
 use crate::model::notification::Notification;
 use crate::model::subscriber::SubscriberRequest;
-use crate::service::notification::NotificationRepository;
+use crate::service::notification::NotificationService;
